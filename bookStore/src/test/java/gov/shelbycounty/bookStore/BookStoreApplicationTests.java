50447b312bfb10197f3ab769552fbf740f5540e5
Avinash Chandragiri <avinashkumargoud@gmail.com>
2022-11-02 15:08:01 -0500

    new spring project

---
+++ bookStore/src/test/java/gov/shelbycounty/bookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.shelbycounty.bookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
